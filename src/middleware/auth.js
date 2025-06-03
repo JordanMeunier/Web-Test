a619fe904afeecc52696d4583d66b483f69d4496
Jordan Meunier <jordan.meunier@epitech.eu>
2025-06-03 16:19:37 +0200

    feat: invalid token, error

---
+++ src/middleware/auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader)
+        return res.status(498).json({ msg: 'No token , authorization denied' });
+    const token = authHeader.split(' ')[1];
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        return res.status(498).json({ msg: 'Token is not valid' });
+    }
+};
