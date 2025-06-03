40c84cf61b35513517809770b534be612eedfb4a
Jordan Meunier <jordan.meunier@epitech.eu>
2025-06-03 17:53:49 +0200

    fix: making it more readable

--- src/routes/posts/posts.query.js
+++ src/routes/posts/posts.query.js
@@ -1,7 +1,10 @@
 const db = require('../../config/db');
 
 exports.get_posts = (user_id, cb) =>
-  db.execute('SELECT * FROM post WHERE user_id = ?', [user_id], cb);
+  db.execute('SELECT * FROM post WHERE user_id = ?',
+    [user_id],
+    cb
+  );
 
 exports.create_post = (title, body, image_url, user_id, cb) =>
   db.execute(
@@ -18,41 +21,28 @@ exports.updatePost = (id, title, body, image_url, user_id, cb) =>
   );
 
 exports.deletePost = (id, cb) =>
-  db.execute('DELETE FROM post WHERE id = ?', [id], cb);
+  db.execute('DELETE FROM post WHERE id = ?',
+    [id],
+    cb
+  );
 
 exports.get_post_details = (post_id, cb) => {
   const postQuery = `
-    SELECT 
-      post.id, 
-      post.title, 
-      post.body, 
-      post.image_url, 
-      user.id AS user_id, 
-      user.username
-    FROM post
-    JOIN user ON post.user_id = user.id
-    WHERE post.id = ?
-  `;
+    SELECT post.id, post.title, post.body, post.image_url, user.id AS user_id, user.username FROM post JOIN user ON post.user_id = user.id WHERE post.id = ?`;
 
-  db.execute(postQuery, [post_id], (err, postResults) => {
+  db.execute(postQuery,
+    [post_id],
+    (err, postResults) => {
     if (err)
       return cb(err);
     if (postResults.length === 0)
       return cb(null, null);
     const post = postResults[0];
     const commentsQuery = `
-      SELECT 
-        comment.id, 
-        comment.comment, 
-        comment.date, 
-        user.id AS user_id, 
-        user.username
-      FROM comment
-      JOIN user ON comment.user_id = user.id
-      WHERE comment.post_id = ?
-      ORDER BY comment.date ASC
-    `;
-    db.execute(commentsQuery, [post_id], (err, commentsResults) => {
+      SELECT comment.id, comment.comment, comment.date, user.id AS user_id, user.username FROM comment JOIN user ON comment.user_id = user.id WHERE comment.post_id = ? ORDER BY comment.date ASC`;
+    db.execute(commentsQuery,
+      [post_id],
+      (err, commentsResults) => {
       if (err)
         return cb(err);
       post.comments = commentsResults || [];
