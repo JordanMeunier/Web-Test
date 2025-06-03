97fa20c1986f666c53f0be6230c1b4b39580d9d1
Jordan Meunier <jordan.meunier@epitech.eu>
2025-06-03 16:20:27 +0200

    feat: post route

---
+++ src/routes/posts/posts.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth');
+const post = require('./posts.query');
+
+router.get('/', auth, (req, res) => {
+    post.get_posts(req.user.id, (err, results) => {
+        if (err)
+            return res.status(500).json({ msg: 'Internal server error' });
+        res.json(results);
+    });
+});
+
+router.post('/', auth, (req, res) => {
+    const { title, body, image_url } = req.body;
+    if (!title || !body || !image_url)
+        return res.status(400).json({ msg: 'Bad parameter' });
+
+    post.create_post(title, body, image_url, req.user.id, (err, result) => {
+        if (err)
+            return res.status(500).json({ msg: 'Internal server error' });
+
+        res.status(201).json({
+            id: result.insertId,
+            title,
+            body,
+            image_url,
+            user_id: req.user.id
+        });
+    });
+});
+
+router.put('/:id', auth, (req, res) => {
+    const { title, body, image_url } = req.body;
+    if (!title || !body || !image_url)
+        return res.status(400).json({ msg: 'Bad parameter' });
+
+    post.updatePost(req.params.id, title, body, image_url, req.user.id, (err, result) => {
+        if (err)
+            return res.status(500).json({ msg: 'Internal server error' });
+        if (result.affectedRows === 0)
+            return res.status(404).json({ msg: 'Not found' });
+
+        res.json({
+            id: parseInt(req.params.id),
+            title,
+            body,
+            image_url,
+            user_id: req.user.id
+        });
+    });
+});
+
+router.delete('/:id', auth, (req, res) => {
+    post.delete_post(req.params.id, (err, result) => {
+        if (err)
+            return res.status(500).json({ msg: 'Internal server error' });
+        if (result.affectedRows === 0)
+            return res.status(404).json({ msg: 'Not found' });
+        res.json({ msg: `Successfully deleted record number : ${req.params.id}` });
+    });
+});
+module.exports = router;
